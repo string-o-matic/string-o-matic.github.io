020cf89af1901c80005a375ee50e319a780a0265
davemorrissey <davemorrissey@gmail.com>
2025-06-07 22:20:06 +0100

    Initial commit

---
+++ precache-manifest.2559fcc7b3f61a0627601ed65cec481e.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+  {
+    "revision": "85b13cdca40ce601a2a0",
+    "url": "/static/js/main.3cb3963b.js"
+  },
+  {
+    "revision": "85b13cdca40ce601a2a0",
+    "url": "/static/css/main.4f9ec248.css"
+  },
+  {
+    "revision": "472a10463f7d5fbb2824b2c4711f86c4",
+    "url": "/index.html"
+  }
+];
